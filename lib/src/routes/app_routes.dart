6bd2c56053ef05094442c51d2214c8d617051754
m16-omar <m16-omar@users.noreply.github.com>
2026-08-11 11:58:44 +0100

    fix: restore initial launch location to Splash Screen for full onboarding and authentication flow

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -82,7 +82,7 @@ class MainShellScaffold extends StatelessWidget {
 
 final appRouter = GoRouter(
   navigatorKey: rootNavigatorKey,
-  initialLocation: RouteNames.home,
+  initialLocation: RouteNames.splash,
   routes: [
     GoRoute(
       path: RouteNames.splash,
