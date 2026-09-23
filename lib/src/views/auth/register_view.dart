9889218017d635475d6c426c69b2a1beb6f64482
m16-omar <m16-omar@users.noreply.github.com>
2026-09-23 12:02:41 +0100

    fix: replace cell-data G icon with authentic 4-color vector Google logo

--- lib/src/views/auth/register_view.dart
+++ lib/src/views/auth/register_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../../const/app_colors.dart';
@@ -413,13 +415,38 @@ class _RegisterViewState extends ConsumerState<RegisterView> {
                     ],
                   ),
                   const SizedBox(height: 18),
+                  // Social Sign-up Buttons
                   Row(
                     children: [
-                      Expanded(child: _SocialButton(label: 'Google', icon: Icons.g_mobiledata_rounded)),
+                      Expanded(
+                        child: _SocialButton(
+                          label: 'Google',
+                          icon: SvgPicture.string(_googleLogoSvg, width: 20, height: 20),
+                          onTap: () {
+                            SnackbarHelper.showSuccess(context, 'Google Sign-Up coming soon');
+                          },
+                        ),
+                      ),
                       const SizedBox(width: 12),
-                      Expanded(child: _SocialButton(label: 'Apple', icon: Icons.apple_rounded)),
+                      Expanded(
+                        child: _SocialButton(
+                          label: 'Apple',
+                          icon: const FaIcon(FontAwesomeIcons.apple, color: Colors.white, size: 20),
+                          onTap: () {
+                            SnackbarHelper.showSuccess(context, 'Apple Sign-Up coming soon');
+                          },
+                        ),
+                      ),
                       const SizedBox(width: 12),
-                      Expanded(child: _SocialButton(label: 'Facebook', icon: Icons.facebook_rounded)),
+                      Expanded(
+                        child: _SocialButton(
+                          label: 'Facebook',
+                          icon: const FaIcon(FontAwesomeIcons.facebook, color: Color(0xFF1877F2), size: 20),
+                          onTap: () {
+                            SnackbarHelper.showSuccess(context, 'Facebook Sign-Up coming soon');
+                          },
+                        ),
+                      ),
                     ],
                   ),
                   const SizedBox(height: 24),
@@ -566,30 +593,53 @@ class _DarkField extends StatelessWidget {
   }
 }
 
+const String _googleLogoSvg = '''
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+  <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
+  <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/>
+  <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/>
+  <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/>
+  <path fill="none" d="M0 0h48v48H0z"/>
+</svg>
+''';
+
 class _SocialButton extends StatelessWidget {
   final String label;
-  final IconData icon;
-  const _SocialButton({required this.label, required this.icon});
+  final Widget icon;
+  final VoidCallback? onTap;
+
+  const _SocialButton({
+    required this.label,
+    required this.icon,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.white.withValues(alpha: 0.08),
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: Colors.white24),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, color: Colors.white, size: 22),
-          const SizedBox(width: 6),
-          Text(
-            label,
-            style: GoogleFonts.inter(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.w500),
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white.withValues(alpha: 0.08),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.white24),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            const SizedBox(width: 8),
+            Text(
+              label,
+              style: GoogleFonts.inter(
+                color: Colors.white,
+                fontSize: 12,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
